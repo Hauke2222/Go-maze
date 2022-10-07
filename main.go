7d61b2f2353287ad0dcddaf4396628065bcae502
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2022-10-07 15:56:33 +0200

    add move function

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+var row int = 0
+var column int = 1
+
 var maze = [10][10]string{
 	{"*", "S", "*", "*", "*", "*", "*", "*", "*", "*"},
 	{"*", " ", "*", "*", " ", " ", " ", " ", " ", "*"},
@@ -19,13 +22,38 @@ func printMaze() {
 	for _, i := range maze {
 		fmt.Println(i)
 	}
+	println("---------------------")
 }
 
-func move() {
+func move(row, column *int) int {
 
+	if maze[*row+1][*column] != "*" { // Down
+		maze[*row][*column] = "V"
+		maze[*row+1][*column] = "S"
+		*row += 1
+	} else if maze[*row-1][*column] != "*" { //	Up
+		maze[*row][*column] = "V"
+		maze[*row-1][*column] = "S"
+		*row -= 1
+	} else if maze[*row][*column+1] != "*" { // Right
+		maze[*row][*column] = "V"
+		maze[*row][*column+1] = "S"
+		*column += 1
+	} else if maze[*row][*column-1] != "*" { // Left
+		maze[*row][*column] = "V"
+		maze[*row][*column-1] = "S"
+		*column -= 1
+	} else {
+		return 1
+	}
+
+	printMaze()
+	// return 1
+	return move(row, column)
 }
 
 func main() {
 	printMaze()
-	move()
+	move(&row, &column)
+	return
 }
